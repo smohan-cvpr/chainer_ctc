5ae0c09dd30c42309d54a3358ff2fac37774347d
smohan-cvpr <62891231+smohan-cvpr@users.noreply.github.com>
2021-04-05 12:17:23 +0800

    Update install_warp-ctc.sh

--- install_warp-ctc.sh
+++ install_warp-ctc.sh
@@ -2,9 +2,9 @@
 
 mkdir -p ext/warp-ctc
 cd ext/warp-ctc
-# git clone https://github.com/baidu-research/warp-ctc.git .
-# Temporary fix for newer GPUs. See https://github.com/espnet/espnet/issues/733
-git clone https://github.com/jnishi/warp-ctc .
+# git clone https://github.com.cnpmjs.org/baidu-research/warp-ctc.git .
+# Temporary fix for newer GPUs. See https://github.com.cnpmjs.org/espnet/espnet/issues/733
+git clone https://github.com.cnpmjs.org/jnishi/warp-ctc .
 mkdir build
 cd build
 cmake ../
